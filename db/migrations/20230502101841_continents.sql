4865ca49fb9da6b681600c48f3af4bb041245aa2
Inseok Chang <inni.chang@gmail.com>
2023-05-03 17:55:39 +0900

    db modification

--- db/migrations/20230502101841_continents.sql
+++ db/migrations/20230502101841_continents.sql
@@ -1,9 +1,7 @@
 -- migrate:up
 CREATE TABLE continents(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    continent VARCHAR(100) NOT NULL,
-    country_id INT NOT NULL,
-    FOREIGN KEY(country_id) REFERENCES countries(id)
+    continent VARCHAR(100) NOT NULL
 )
 -- migrate:down
 DROP TABLE continents
