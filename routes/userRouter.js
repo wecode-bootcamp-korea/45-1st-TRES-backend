666f51b6af9b744238cab15a546268793139f585
DoorWinBell <msj102525@gmail.com>
2023-05-04 21:04:22 +0900

    RestFul

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-// const { authorization } = require('../utils/authorization');
 const router = express.Router();
 
 router.post('/check', userController.userEmailCheck);
