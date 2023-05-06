55117a7b4d44626de651f3c898dd5e9eb0ed5696
Inseok Chang <inni.chang@gmail.com>
2023-05-06 13:45:30 +0900

    main-page-call-product

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,6 @@ const productService = require('../services/productService');
 const getMainPage = async (req, res) => {
     try{
         const mainPage = await productService.getMainPage();
-        console.log(mainPage);
         return res.status(200).json({ mainPage });
     } catch(err){
         return res.status(err.statusCode || 400).json({ message:err.message });
