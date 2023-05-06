5210044a018673296554c37ecb5b978131fceef1
DoorWinBell <msj102525@gmail.com>
2023-05-06 18:43:25 +0900

    signup refacktoring

--- routes/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/userController');
-const { authorization } = require('../utils/authorization');
-const router = express.Router();
-
-router.get('/', authorization);
-
-module.exports = { router };
