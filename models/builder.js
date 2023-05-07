edf47cd8e084b46a7868b37734d2503bfdcac505
하지현 <hazzi@hajihyeon-ui-MacBookAir.local>
2023-05-07 19:47:53 +0900

    ADD:error-handling

---
+++ models/builder.js
@@ -0,0 +1,62 @@
+function filterBuilder(countryId, spiceLevel, allergyId, meatId) {
+  let conditionArr = [];
+
+  if (countryId) {
+    conditionArr.push(`f.country_id = ${countryId}`);
+  }
+
+  if (spiceLevel) {
+    conditionArr.push(`f.spice_level = ${spiceLevel}`);
+  }
+
+  if (allergyId) {
+    conditionArr.push(`f.spice_level = ${allergyId}`);
+  }
+
+  if (meatId) {
+    conditionArr.push(`f.spice_level = ${meatId}`);
+  }
+
+  let whereCondition = "";
+  if (conditionArr.length > 0) {
+    whereCondition = `WHERE ${conditionArr.join(" AND ")}`;
+  }
+  return whereCondition;
+}
+
+function orderByBuilder(orderBy) {
+  let orderQuery = "";
+  switch (orderBy) {
+    case "priceAsc":
+      orderQuery = "ORDER BY f.price ASC, f.id ASC";
+      break;
+    case "priceDesc":
+      orderQuery = "ORDER BY f.price DESC, f.id DESC";
+      break;
+    case "best":
+      orderQuery = "ORDER BY likes_count DESC";
+      break;
+    default:
+      orderQuery = "ORDER BY f.id";
+      break;
+  }
+  return orderQuery;
+}
+
+function limitBuilder(limit, offset) {
+  if (!limit) {
+    limit = 12;
+  }
+
+  if (!offset) {
+    offset = 0;
+  }
+
+  return `LIMIT ${limit} OFFSET ${offset}`;
+}
+
+module.exports = {
+  filterBuilder,
+  orderByBuilder,
+  limitBuilder,
+};
