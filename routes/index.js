45df55c0367147efb15a36b51f513b18485f6bf0
DoorWinBell <msj102525@gmail.com>
2023-05-07 21:25:22 +0900

    delete signup

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const productRouter = require("./productRouter");
 
 const router = express.Router();
 
-const userRouter = require('./userRouter');
-const orderRouter = require('./orderRouter');
+const orderRouter = require("./orderRouter");
 
-router.use('/users', userRouter.router);
-router.use('/order', orderRouter.router);
-
+router.use("/users", userRouter.router);
+router.use("/order", orderRouter.router);
 
 module.exports = router;
