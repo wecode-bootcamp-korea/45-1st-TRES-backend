98908e02176c95dcb21caf35be961d37da303433
DoorWinBell <msj102525@gmail.com>
2023-05-07 20:02:49 +0900

    delete signup

--- utils/validationCheck.js
+++ utils/validationCheck.js
@@ -1,8 +1,8 @@
 const emailValidationCheck = async (email) => {
   // email validation using REGEX
-  const emailValidation = new RegExp('^[a-z]{2,}@[a-z]{2,}.[a-z]{2,}$');
+  const emailValidation = new RegExp("^[a-z]{2,}@[a-z]{2,}.[a-z]{2,}$");
   if (!emailValidation.test(email)) {
-    const err = new Error('EMAIL_IS_NOT_VALID');
+    const err = new Error("EMAIL_IS_NOT_VALID");
     err.statusCode = 409;
     throw err;
   }
@@ -11,9 +11,11 @@ const emailValidationCheck = async (email) => {
 
 const passwordValidationCheck = async (password) => {
   // password validation using REGEX
-  const pwValidation = new RegExp('^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})');
+  const pwValidation = new RegExp(
+    "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
+  );
   if (!pwValidation.test(password)) {
-    const err = new Error('PASSWORD_IS_NOT_VALID');
+    const err = new Error("PASSWORD_IS_NOT_VALID");
     err.statusCode = 409;
     throw err;
   }
