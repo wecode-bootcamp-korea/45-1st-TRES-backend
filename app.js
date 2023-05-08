7f5ece2c6feab640190205ad61f24f40ca1724d4
Inseok Chang <inni.chang@gmail.com>
2023-05-08 12:32:32 +0900

    main page with resolved conflict + postman test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const routes = require("./routes");
 const { globalErrorHandler } = require("./utils/error");
 const dataSource = require("./models/dataSource");
 
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
