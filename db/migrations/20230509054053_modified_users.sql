c5b1822a128123d5ee8aa606a52148608e1f6d7d
DoorWinBell <msj102525@gmail.com>
2023-05-10 10:24:30 +0900

    migration file

--- db/migrations/20230509054053_modified_users.sql
+++ db/migrations/20230509054053_modified_users.sql
@@ -1,7 +1,4 @@
 -- migrate:up
--- modified_order_number_email
-ALTER TABLE orders MODIFY order_number VARCHAR(300) NULL;
-
 ALTER TABLE users MODIFY email VARCHAR(400) NOT NULL UNIQUE;
 
 -- migrate:down
