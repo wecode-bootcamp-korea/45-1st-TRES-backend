a47ee91f3c65c4de099cd504ccbe1cec9d1231aa
Inseok Chang <inni.chang@gmail.com>
2023-05-10 12:54:09 +0900

    HOTFIX: Migration - order status relation

--- services/orderService.js
+++ services/orderService.js
@@ -5,11 +5,12 @@ const modifyOrderCount = async (foodId, quantity, userId) => {
 };
 
 const deleteOrder = async(deleteOrderItem, userId) =>{
-
   const food_id = [];
+
   deleteOrderItem.map((item)=>{
     food_id.push(item.foodId)
   })
+
   const isExist = await orderDao.checkDeleteQuery(food_id, userId);
   
   const inputLength = food_id.length;
@@ -18,8 +19,8 @@ const deleteOrder = async(deleteOrderItem, userId) =>{
   if(inputLength !== isExistLength) return false  
 
   await orderDao.deleteOrderItems(food_id, userId);
+  
   return true;
-
 };
 
 module.exports = { modifyOrderCount, deleteOrder };
